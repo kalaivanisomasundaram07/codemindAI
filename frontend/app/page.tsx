186f336cdd9984c478936ec11a08facee4b4bb12
kalaivanisomasundaram07 <kalaivanisomasundaram07@gmail.com>
2026-05-16 23:57:31 +0530

    connected frontend to render backend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -16,7 +16,7 @@ print(greet("World"))
 
   const handleAskAI = async () => {
     try {
-      const res = await fetch("http://127.0.0.1:5000/chat", {
+      const res = await fetch("https://codemindai-0eu9.onrender.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
